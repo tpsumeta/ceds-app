8fc93a5b4b4aa07a7397fe24970e7b76e9c72bd1
tpsumeta <tpsumeta@gmail.com>
2016-09-19 18:53:21 +0700

    init

---
+++ app/pages/home/home.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { LoadingController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+
+  searchQuery: string = '';
+  items;
+  api:string = 'http://ceds.dusit.ac.th/api/getReport?code_faculty=all&code_department=all&staff_type=all&code_person=&location_id=all&building_id=all&floor_id=all&room_id=all&n_id=all&durablegoods_id=all&type_id=all&brend_id=all&g_id=all&_=1474276776066';
+  posts;
+
+  constructor(public navCtrl: NavController,private http: Http,public loadingCtrl: LoadingController) {
+      
+       this.initializeItems();
+    
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
+
+  initializeItems() {
+     this.http.get(this.api).map(res => res.json()).subscribe(data => {
+       //this.items = data.data;
+       this.presentLoading();
+       this.posts = data.data;
+    });
+  }
+
+
+
+
+getItems(ev: any) {
+  // Reset items back to all of the items
+ let val = ev.target.value;
+ if (val && val.trim() != '') {
+     console.log('val',val.length);
+     this.items = this.posts;
+
+     this.items = this.items.filter((item) => {
+         console.log('item',item);
+      return (item.serial.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    });
+
+  this.presentLoading();
+
+  }
+
+
+}
+
+
+}
