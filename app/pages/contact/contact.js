92cc13a5f1988b8255d1261daf18d8988a1a36d0
tpsumeta <tpsumeta@gmail.com>
2016-09-20 11:03:21 +0700

    version 0.1

--- app/pages/contact/contact.js
+++ app/pages/contact/contact.js
@@ -10,15 +10,48 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var ionic_angular_1 = require('ionic-angular');
+var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
+var ionic_angular_2 = require('ionic-angular');
 var ContactPage = (function () {
-    function ContactPage(navCtrl) {
+    function ContactPage(navCtrl, http, loadingCtrl) {
         this.navCtrl = navCtrl;
+        this.http = http;
+        this.loadingCtrl = loadingCtrl;
+        this.searchQuery = '';
+        this.api = 'http://ceds.dusit.ac.th/api/getReport?code_faculty=all&code_department=all&staff_type=all&code_person=&location_id=all&building_id=all&floor_id=all&room_id=all&n_id=all&durablegoods_id=all&type_id=all&brend_id=all&g_id=all&_=1474276776066';
+        this.initializeItems();
     }
+    ContactPage.prototype.initializeItems = function () {
+        var _this = this;
+        this.http.get(this.api).map(function (res) { return res.json(); }).subscribe(function (data) {
+            _this.posts = data.data;
+        });
+    };
+    ContactPage.prototype.presentLoading = function () {
+        var loader = this.loadingCtrl.create({
+            content: "กรุณารอสักครู่...",
+            duration: 300
+        });
+        loader.present();
+    };
+    ContactPage.prototype.getItems = function (ev) {
+        var val = ev.target.value;
+        this.presentLoading();
+        if (val && val.trim() !== '') {
+            this.items = this.posts;
+            this.items = this.items.filter(function (item) {
+                console.log('item', item);
+                return (item.first_name_tha.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            });
+        }
+        ;
+    };
     ContactPage = __decorate([
         core_1.Component({
             templateUrl: 'build/pages/contact/contact.html'
         }), 
-        __metadata('design:paramtypes', [ionic_angular_1.NavController])
+        __metadata('design:paramtypes', [ionic_angular_1.NavController, http_1.Http, ionic_angular_2.LoadingController])
     ], ContactPage);
     return ContactPage;
 }());
--- app/pages/contact/contact.ts
+++ app/pages/contact/contact.ts
@@ -1,26 +1,53 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SQLite } from 'ionic-native';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/contact/contact.html'
 })
 export class ContactPage {
-  constructor(public navCtrl: NavController) {
 
-    // let db = new SQLite();
-    // db.openDatabase({
-    //   name: 'data.db',
-    //   location: 'default' // the location field is required
-    // }).then(() => {
-    //   db.executeSql('create table danceMoves(name VARCHAR(32))', {}).then(() => {
-    //     console.log('create table success');
-    //   }, (err) => {
-    //     console.error('Unable to execute sql: ', err);
-    //   });
-    // }, (err) => {
-    //   console.error('Unable to open database: ', err);
-    // });
+  searchQuery: string = '';
+  items:any;
+  api:string = 'http://ceds.dusit.ac.th/api/getReport?code_faculty=all&code_department=all&staff_type=all&code_person=&location_id=all&building_id=all&floor_id=all&room_id=all&n_id=all&durablegoods_id=all&type_id=all&brend_id=all&g_id=all&_=1474276776066';
+  posts:JSON;
+
+
+  constructor(public navCtrl: NavController,private http: Http,public loadingCtrl: LoadingController) {
+
+       this.initializeItems();
+
+  }
+
+  initializeItems() {
+     this.http.get(this.api).map(res => res.json()).subscribe(data => {
+       this.posts = data.data;
+    });
+  }
+
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "กรุณารอสักครู่...",
+      duration: 300
+    });
+    loader.present();
+  }
+
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+   let val = ev.target.value;
+   this.presentLoading();
+   if (val && val.trim() !== '') {
+       this.items = this.posts;
+
+       this.items = this.items.filter((item) => {
+           console.log('item',item);
+        return (item.first_name_tha.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    };
 
 
   }
